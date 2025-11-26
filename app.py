11d04a4cc502393f082c6c981411a80bab198ae0
Joyuv <givanilsonmariz7@outlook.com>
2025-11-26 20:37:10 -0300

    modelagem do banco e rota de adicionar veiculos

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify
 from sqlalchemy import select
-from models.modelos import Base, db, engine, Veiculo
+from models.modelos import Base, db, engine, Veiculo, Frota
 
 app = Flask(__name__)
 
@@ -15,10 +15,28 @@ def index():
         return "Tudo funcional!"
 
 
-@app.route("/veiculo/list", methods=["POST"])
+@app.route("/veiculo/list", methods=["GET"])
 def veiculos():
     if request.method == "POST":
         list = []
         for veiculo in db.scalars(select(Veiculo)):
             list.append(veiculo)
         return jsonify({"message": "Requisicao autorizada", "list": list})
+
+
+@app.route("/veiculo/add", methods=["POST"])
+def adicionar_veiculo():
+    form = request.json()
+    if form.get("placa") is not None and form.get("frota_id") is not None:
+        data = {"placa": form.get("placa"), "frota_id": form.get("frota_id")}
+        veiculo = Veiculo(placa=str(data["placa"], frota_id=int(data["frota_id"])))
+        db.add(veiculo)
+        db.commit()
+        return jsonify({"message": "Veiculo adicionado com sucesso!"})
+    else:
+        return jsonify({"message": "Erro, falta informacoes"}, 417)
+
+
+@app.route("/veiculo/update", methods=["POST"])
+def atualizar_veiculo():
+    pass
--- models/modelos.py
+++ models/modelos.py
@@ -1,5 +1,6 @@
-from sqlalchemy import String, create_engine
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, Session
+from sqlalchemy import String, create_engine, ForeignKey
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, Session, relationship
+from typing import List, Optional
 
 engine = create_engine("sqlite:///banco.db", echo=True)
 db = Session(engine)
@@ -14,4 +15,21 @@ class Veiculo(Base):
 
     id: Mapped[int] = mapped_column(primary_key=True)
     placa: Mapped[str] = mapped_column(String(7), unique=True)
+    frota_id: Mapped[int] = mapped_column(ForeignKey("frotas.id"))
+
+    frota: Mapped["Frota"] = relationship(back_populates="veiculos")
     # o resto das colunas
+
+
+class Frota(Base):
+    __tablename__ = "frotas"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    nome: Mapped[str] = mapped_column(String(255))
+    lista_veiculos: Mapped[Optional[List[int]]] = mapped_column(
+        ForeignKey("veiculos.id")
+    )
+
+    veiculos: Mapped[Optional[List["Veiculo"]]] = relationship(
+        back_populates="frota", cascade="all, delete-orphan"
+    )
